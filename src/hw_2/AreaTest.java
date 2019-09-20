2b0360413120d90b3abc10653de82aceb1d29b50
Aleksandr Svistoplyasov <alex@svist.com>
2019-09-20 11:59:13 +0300

    added exceptions tests

--- src/hw_2/AreaTest.java
+++ src/hw_2/AreaTest.java
@@ -4,28 +4,57 @@ import static java.lang.Math.*;
 import static org.junit.Assert.*;
 
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 class AreaTest {
     @Test
-    void areaCircle() {
-        double r = random() * 100;
+    public void areaCircle() {
+        double r = random() * 100 + 0.1;
         Circle circle = new Circle(r);
         assertEquals(PI * r * r, circle.area(), pow(0.1, 10));
     }
 
     @Test
-    void areaSquare() {
-        double a = random() * 100;
+    public void areaSquare() {
+        double a = random() * 100 + 0.1;
         Square square = new Square(a);
         assertEquals(a * a, square.area(), pow(0.1, 10));
     }
 
     @Test
-    void areaRectangle() {
-        double a = random() * 100;
-        double b = random() * 100;
+    public void areaRectangle() {
+        double a = random() * 100 + 0.1;
+        double b = random() * 100 + 0.1;
         Rectangle rectangle = new Rectangle(a,b);
         assertEquals(a * b, rectangle.area(), pow(0.1, 10));
+    }
+
+    @Test
+    public void circleIllegalArgumentsExpectation() throws IllegalArgumentException{
+        try {
+            Circle circle = new Circle(0);
+            Assert.fail("Expected IllegalArgumentsExpectation");
+        }catch (IllegalArgumentException thrown){}
+    }
+
+    @Test
+    public void squareIllegalArgumentsExpectation() throws IllegalArgumentException{
+        try {
+            Square square = new Square(-1);
+            Assert.fail("Expected IllegalArgumentsExpectation");
+        }catch (IllegalArgumentException thrown){}
+
+
+    }
+
+    @Test
+    public void rectangleIllegalArgumentsExpectation() throws IllegalArgumentException{
+        try {
+            Rectangle rectangle = new Rectangle(0, -1);
+            Assert.fail("Expected IllegalArgumentsExpectation");
+        }catch (IllegalArgumentException thrown){}
+
+
     }
 }
